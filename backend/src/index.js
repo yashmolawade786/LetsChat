03a9f3cf235d06e341f7226bbe5d37b75ef4f411
yashmolawade <yashmolawade06@gmail.com>
2025-07-21 21:57:41 +0530

    change2

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,20 +18,23 @@ const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://letschat-1-mnk2.onrender.com",
+];
 
-
-const allowedOrigins = ['https://letschat-1-mnk2.onrender.com'];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin)) {
-      callback(null, origin);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
